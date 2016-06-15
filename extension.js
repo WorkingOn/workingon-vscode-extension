6b2e3614a0ae475de842e70ed80600bf876fa694
gdiab <me@georgediab.com>
2016-06-15 07:17:54 -0700

    tracer bullets

--- extension.js
+++ extension.js
@@ -1,3 +1,5 @@
+'use strict';
+
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 var vscode = require('vscode');
@@ -13,14 +15,26 @@ function activate(context) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    var disposable = vscode.commands.registerCommand('extension.sayHello', function () {
+    let helloDisposable = vscode.commands.registerCommand('extension.sayHello', function () {
         // The code you place here will be executed every time your command is executed
 
         // Display a message box to the user
         vscode.window.showInformationMessage('Hello World!');
     });
 
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(helloDisposable);
+
+    let updateTokenDisposable = vscode.commands.registerCommand('extension.updateToken', () => {
+        vscode.window.showInformationMessage('Update token Initiated.')
+    });
+
+    context.subscriptions.push(updateTokenDisposable);
+
+    let updateWorkingOnDisposable = vscode.commands.registerCommand('extension.updateWorkingOn', () => {
+        vscode.window.showInformationMessage('Update WorkingOn Initiated.')
+    });
+
+    context.subscriptions.push(updateWorkingOnDisposable);
 }
 exports.activate = activate;
 
