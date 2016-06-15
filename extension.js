bbe1a3d8d3aa2c79b73b9cc81b232cebb4216b2b
gdiab <me@georgediab.com>
2016-06-15 09:15:31 -0700

    remove yo boilerplate code

--- extension.js
+++ extension.js
@@ -10,19 +10,6 @@ function activate(context) {
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "workingon-vscode-extension" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let helloDisposable = vscode.commands.registerCommand('extension.sayHello', function () {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
-
-    context.subscriptions.push(helloDisposable);
 
     let updateTokenDisposable = vscode.commands.registerCommand('extension.updateToken', () => {
         vscode.window.showInformationMessage('Update token Initiated.')
